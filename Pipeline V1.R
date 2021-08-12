455092970a5e6b836aa365941f228856da8a43ac
Oceana Tavasieff <oceana@ucsb.edu>
2021-08-12 16:31:23 -0700

    beginning to create a master pipeline. Very early work annotating Richard et. al dataset

--- Pipeline V1.R
+++ Pipeline V1.R
@@ -12,21 +12,35 @@ library(phyloseq)
 #Function: Return matching species names#
 ####
 
-match_names <- function()
-erlandson_names <- erlandson_metadata %>% select (Tree_site, Ecto_ID, Ecto_spp, Tip) %>% mutate(ref_name = numeric(nrow(.)))
+ref_ID <- readRDS("master_ID_df.rds")
 
+#match_names will compare species binomials assigned in your data with an expanding reference list (ref_ID). You input a corresponding OTU and binomial list ("OTU" and "ID") and it will spit out any overlapping OTU with our existing reference list. Only reports exact matches. For instance, Amanita muscaria clone. J459 will not match with Amanita muscaria. match_names also binds the rows of your input data to the masterlist, so is intended to be used concurrently. For the moment renaming the overlapping OTU's has to happen manually.
 
-for(i in 1:nrow(erlandson_names)) {
-  for(j in 1:nrow(ref_ID)) {
-    if(erlandson_names$Ecto_ID[i] == ref_ID$ID[j]) {
-      erlandson_names$ref_name[i] = ref_ID$Species[j] 
-      if(erlandson_names$ref_name[i] == 0) {
-        erlandson_names$ref_name[i] = erlandson_names$Ecto_spp[i]
-      }
-      
-    }
-    
-  }
+match_names <- function(OTU, ID, ref_ID){
+  input_df = data.frame(OTU,ID)
+  input_df = unique(input_df)
+  input_df$ref_name = rep(0)
+  for(i in 1:nrow(input_df)) {
+    for(j in 1:nrow(ref_ID)) {
+      if(input_df$ID[i] == ref_ID$ID[j]) {
+        input_df$ref_name[i] = ref_ID$OTU[j]
+        print(paste("Replace ID:", input_df$ID[i], "with", ref_ID$OTU[j]))}}}
+  ref_ID <- bind_rows(ref_ID, input_df) %>% distinct(.)
+ # print(unique(input_df$ref_name))
+  ref_ID <<- ref_ID
 }
 
-unique(erlandson_names$ref_name)
+#match_names(test$Ecto_spp, test$Ecto_ID, ref_ID)
+#erlandson_OTU_combined <- erlandson_OTU %>% select(order(colnames(.))) %>% 
+ # rename("17" = Cortinarius1,
+  #       "3" = Cenococcum_geophilum)
+
+####
+
+
+
+
+
+
+
+
