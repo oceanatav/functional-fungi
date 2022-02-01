bc8b71618acead51cc2143ec23c67d6774acb077
Oceana Tavasieff <oceana@ucsb.edu>
2022-02-01 13:42:30 -0800

    work up to 2/1, before updating match_names

--- Pipeline V3.R
+++ Pipeline V3.R
@@ -7,11 +7,14 @@ library(tidyverse)
 
 ####
 #a.Clean up and consolidate similar spp names in ref_ID####
-####
+
+
+ref_ID<-readRDS(file = "master_ID_df.rds")
+
 # here is Laura's building a table # 01/18/2022
-clean_ref <- function(ref_ID) {
+clean_ref <- function(reference_list_OTU_ID) {
   
-  ref_ID_noNAs = ref_ID %>% filter(ID != "NA")
+  ref_ID_noNAs = reference_list_OTU_ID %>% filter(ID != "NA")
 
 output = tibble()
 
@@ -23,10 +26,13 @@ for(i in 1:nrow(ref_ID_noNAs)) {
   
 }
 
-new_ref_ID <<- output %>% distinct()
+ref_ID_new <<- output %>% distinct()
 
 }
+
+
 clean_ref(ref_ID)
+
 ####
 #b.Return matching species names####
 ####
@@ -41,7 +47,7 @@ match_names <- function(OTU, ID, ref_ID, site){
     for(j in 1:nrow(ref_ID)) {
       if(input_df$ID[i] == ref_ID$ID[j]) {
         input_df$ref_name[i] = ref_ID$OTU[j]
-        # print(paste("Replace ID:", input_df$ID[i], "with", ref_ID$OTU[j]))
+        print(paste("Replace ID:", input_df$ID[i], "with", ref_ID$OTU[j]))
       }
       if(input_df$ref_name[i] == 0){
         input_df$ref_name[i] = input_df$OTU[i]
@@ -72,8 +78,9 @@ match_names <- function(OTU, ID, ref_ID, site){
 
 #Tejon
 tejon_spp<-readRDS("NMDS_4_input/tejon_spp_list.rds") %>% unite(ID, Genus, Species, sep=" ") %>% rename(OTU = OUT_MatchwMendo)
-match_names(tejon_spp$OTU, tejon_spp$ID, new_ref_ID, "tejon")
+match_names(tejon_spp$OTU, tejon_spp$ID, ref_ID_new, "tejon")
 tejon_match_names_output <- output_df
+clean_ref(ref_ID_newer)
 
 
 #Richard
@@ -81,10 +88,18 @@ richard_raw<- read_delim("Richard et al OTU table.csv", ";", escape_double = FAL
 richard_spp<-richard_raw %>% select(Taxon) %>%  
 transmute(OTU = str_replace(Taxon, " ", "_"),
           ID = Taxon)
-clean_ref(ref_ID_newer)
-match_names(richard_spp$OTU, richard_spp$ID, new_ref_ID, "richard")
-richard_match_names_output <- output_df
 
+match_names(richard_spp$OTU, richard_spp$ID, ref_ID_new, "richard")
+richard_match_names_output <- output_df
+clean_ref(ref_ID_newer)
+
+####d. Prepare ref_ID to replace all OTU names in future combined matrix####
+
+newest_ref_ID <- clean_ref(ref_ID_newer)
+
+newest_ref_ID$ref_name = ifelse(is.na(newest_ref_ID$ref_name), paste(newest_ref_ID$OTU),paste(newest_ref_ID$ref_name));newest_ref_ID
+
+#tail(newest_ref_ID)
 
 #### 2- Load in data ####
 
@@ -94,9 +109,9 @@ tejon_OTUs <- readRDS("NMDS_4_input/tejon_OTU_matrix.rds")
 tejon_traits <- readRDS("NMDS_4_input/tejon_trait_matrix.rds")
 
 ##Erlandson
-erl_sample <- readRDS("NMDS_4_input/erl_enviro_list.rds")
-erl_OTUs <- readRDS("NMDS_4_input/erl_OTU_matrix.rds")
-erl_traits <- readRDS("NMDS_4_input/erl_trait_matrix.rds")
+erl_sample <- readRDS("NMDS_4_input/erlandson_enviro_list.rds")
+erl_OTUs <- readRDS("NMDS_4_input/erlandson_OTU_matrix.rds")
+erl_traits <- readRDS("NMDS_4_input/erlandson_trait_matrix.rds")
 
 ##Mendocino
 mendo_sample <- readRDS("NMDS_4_input/mendo_enviro_list.rds")
@@ -104,22 +119,89 @@ mendo_OTUs <- readRDS("NMDS_4_input/mendo_OTU_matrix.rds")
 mendo_traits <- readRDS("NMDS_4_input/mendo_trait_matrix.rds")
 
 ##Richard
-rich_sample <- readRDS("NMDS_4_input/rich_enviro_list.rds")
-rich_OTUs <- readRDS("NMDS_4_input/rich_OTU_matrix.rds")
-rich_traits <- readRDS("NMDS_4_input/rich_trait_matrix.rds")
+rich_sample <- readRDS("NMDS_4_input/richard_enviro_list.rds")
+rich_OTUs <- readRDS("NMDS_4_input/richard_OTU_matrix.rds")
+rich_traits <- readRDS("NMDS_4_input/richard_trait_matrix.rds")
 
 
 ####3- Combine Data #### 
 
+####a. Update OTU Names with New Consolidated OTUs ####
 
+#This has to happen in the datasets individually to reduce issues with too many OTUS in the combined matrix
 
-####a. Update OTU Names in Richard and Tejon with New Consolidated OTUs ####
-
-
+update_OTUS <- function(input_dataset, newest_reference_list) {
+  for(i in 1:ncol(input_dataset)){
+    for(j in 1:nrow(newest_reference_list)){
+      if()
+    }
+  }
+}
 
 ####b. Combine Matrices ####
+#Combo OTU
 
+##Make columns and rows
+###Columns
 
+OTU_list <- unique(c(colnames(mendo_OTUs), colnames(tejon_OTUs), colnames(erl_OTUs),colnames(rich_OTUs)))
+###Rows
+tree_names <- c(rownames(mendo_OTUs), rownames(tejon_OTUs), rownames(erl_OTUs), rownames(rich_OTUs))
+#tree_list <- 1:(dim(tejon_OTU_matrix)[1]+dim(mendo_OTU_matrix)[1] + dim(erlandson_OTU_matrix)[1])
+
+##Make empty matrix the with our columns and rows
+
+combo_OTU_matrix <- matrix(rep(0,length(tree_names)*length(OTU_list)),nrow=length(tree_names),ncol=length(OTU_list))
+
+colnames(combo_OTU_matrix) <- OTU_list
+rownames(combo_OTU_matrix) <- tree_names
+combo_OTU_matrix <- combo_OTU_matrix %>% replace(is.na(.), 0)
+
+## Now to fill in the values
+
+for(i in 1:dim(combo_OTU_matrix)[2]){
+  OTU <- colnames(combo_OTU_matrix)[i]
+  
+  # Find OTU data for tejon
+  colnum <- 0
+  for(j in 1:ncol(tejon_OTUs)){
+    if(colnames(tejon_OTUs)[j]==OTU){
+      colnum <- j 	}	}	
+  if(colnum==0){ 	tejon.abun <- rep(0,nrow(tejon_OTUs)) 	} 
+  if(colnum!=0){ 	tejon.abun <- tejon_OTUs[,colnum] }
+  
+  # Find OTU data for mendocino	
+  colnum <- 0
+  for(j in 1:dim(mendo_OTUs)[2]){
+    if(colnames(mendo_OTUs)[j]==OTU){
+      colnum <- j  } 	}	
+  if(colnum==0){	mendo.abun <- rep(0,nrow(mendo_OTUs)) 	} 
+  if(colnum!=0){	mendo.abun <- mendo_OTUs[,colnum]	}
+  
+  #Find OTU data for erlandson (Oceana)
+  
+  colnum <- 0
+  for(j in 1:ncol(erl_OTUs)) {
+    if(colnames(erl_OTUs)[j]==OTU){
+      colnum <- j }	}
+  if(colnum == 0){ erlandson.abun <- rep(0, nrow(erl_OTUs))}
+  if(colnum != 0){erlandson.abun <- erl_OTUs[,colnum]}
+  
+  #Find OTU data for richard (Oceana)
+  
+  colnum <- 0
+  for(j in 1:ncol(rich_OTUs)) {
+    if(colnames(rich_OTUs)[j]==OTU){
+      colnum <- j }	}
+  if(colnum == 0){ richard.abun <- rep(0, nrow(rich_OTUs))}
+  if(colnum != 0){richard.abun <- rich_OTUs[,colnum]}
+  
+  combo_OTU_matrix[,i] <- c(tejon.abun, mendo.abun, erlandson.abun, richard.abun)
+  
+}
+
+# In the above for loop Holly creates an object (colnum). then loops through the columns of a site-specific matrix (eg tejon, mendocino, erlandson's study) and sets the object equal to that value (column name/OTU). if a column name matching that in the site-specific matrix is found, then colnum is not equal to zero and the data from that column (rows) are saved to an object (X.abun). ...
+#All OTU matrices are relativized within-study. All observations of frequency are summed across a tree and divided by the total abundance per tree. 
 
 ####c. Combine Sample Data####
 
